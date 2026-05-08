d19e0a654d14b938fbf8eec94fe3be3f880f1f8a
g <84513918+blowawayyourtroubles@users.noreply.github.com>
2026-05-08 11:16:09 -0400

    define the password function

--- pass.py
+++ pass.py
@@ -4,33 +4,37 @@ import json
 import os
 from datetime import datetime
 
-def contraseña():
 
+def main():
+    resultado = contraseña()
+    print(resultado)
+
+
+def contraseña():
     user_name = input("Insert your user: ")
     web_site = input("Insert the website: ")
+    while True:
+        password = input("Insert your password: ")
 
-    password = "pXPm2Bi0Pc6jEQlL!"
+        lenght_ok = len(password) >= 8
+        has_letter = False
+        has_number = False
+        has_special = False
 
-    lenght_ok = len(password) > 8
+        for char in password:
+            if char.isalpha():
+                has_letter = True
+            elif char.isdigit():
+                has_number = True
+            elif not char.isalnum():
+                has_special = True
 
-    has_letter = False
-    has_number = False
-    has_special = False
-
-
-    for char in password:
-        if char.isalpha():
-            has_letter = True
-        elif char.isdigit():
-            has_number = True
-        elif not char.isalnum():
-            has_special = True
-
-    if lenght_ok and has_letter and has_number and has_special:
-        return f"Your user is {user_name}, website is {web_site}, and your password is {password}."
-    else:
-        print("Wrong password input.")
+        if lenght_ok and has_letter and has_number and has_special:
+            print(f"Your user is {user_name}, website is {web_site}, and your password is {password}.")
+        print("Wrong password input, try again.")
 
 
-resultado = contraseña()
-print(resultado)
+
+
+if __name__ == "__main__":
+    main()
